2d39c5d5b3eea3f6ed94271205e76d4048badee0
merl <merl@kribrum.ru>
2018-07-20 22:32:09 +0300

    Bot commands

--- rust/icfpc2018_lib/src/cmd.rs
+++ rust/icfpc2018_lib/src/cmd.rs
@@ -1,2 +1,28 @@
+use coord::{LinearCoordDiff,CoordDiff}; 
 
-// nothing here yet
+#[derive(Debug)]
+pub enum Error {
+    LinearCoordDiffTooLong,
+}
+
+pub enum BotCommand {
+    Halt,
+    Wait,
+    Flip,
+    SMove{ long: LinearCoordDiff }, 
+    LMove{ short1: LinearCoordDiff, short2: LinearCoordDiff },
+    Fission{ near: CoordDiff, split_m: u8 },
+    Fill{ near: CoordDiff },
+    FusionP{ near: CoordDiff },
+    FusionS{ near: CoordDiff },
+}
+impl BotCommand {
+    pub fn smove(mv: LinearCoordDiff) -> Result<BotCommand,Error> {
+        match mv {
+            LinearCoordDiff::Short { axis, value } | LinearCoordDiff::Long { axis, value } if (value >= -15)&&(value <= 15) => {
+                Ok(BotCommand::SMove{ long: LinearCoordDiff::Long { axis: axis, value: value }})
+            },
+            _  => Err(Error::LinearCoordDiffTooLong),
+        }
+    }
+}
