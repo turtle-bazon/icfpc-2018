fca265ce553d13457a1edee0fa21384374efac0d
Alexey Voznyuk <me@swizard.info>
2018-07-20 20:27:27 +0300

    initial stuff

--- rust/icfpc2018_lib/src/lib.rs
+++ rust/icfpc2018_lib/src/lib.rs
@@ -1,3 +1,35 @@
+
+#[derive(Debug)]
+pub enum Axis { X, Y, Z, }
+
+pub type M = isize;
+
+#[derive(Debug)]
+pub struct Resolution(pub M);
+
+#[derive(Clone, Copy, PartialEq, Eq, Hash, Debug)]
+pub struct Coord {
+    pub x: M,
+    pub y: M,
+    pub z: M,
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Hash, Debug)]
+pub struct CoordDiff(pub Coord);
+
+impl Coord {
+    pub fn add(&self, diff: CoordDiff) -> Coord {
+        Coord {
+            x: self.x + diff.0.x,
+            y: self.y + diff.0.y,
+            z: self.z + diff.0.z,
+        }
+    }
+}
+
+
+// dummy
+
 pub fn dummy() {
 }
 
