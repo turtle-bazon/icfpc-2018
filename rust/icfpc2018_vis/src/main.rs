7c3a8584c1d01cf888a3ef6f19dcf1a3b6c2891b
Alexey Voznyuk <me@swizard.info>
2018-07-20 19:02:49 +0300

    visualizer stuff

--- rust/icfpc2018_vis/src/main.rs
+++ rust/icfpc2018_vis/src/main.rs
@@ -1,6 +1,92 @@
+extern crate gfx_core;
+extern crate env_logger;
+extern crate piston_window;
 extern crate icfpc2018_lib;
+#[macro_use] extern crate log;
+#[macro_use] extern crate clap;
+
+use std::{
+    io,
+    process,
+    path::PathBuf,
+};
+
+use clap::Arg;
+use piston_window::{
+    OpenGL,
+    PistonWindow,
+    WindowSettings,
+    TextureSettings,
+    Glyphs,
+    Event,
+    Input,
+    Button,
+    ButtonArgs,
+    ButtonState,
+    MouseButton,
+    Motion,
+    Key,
+};
 
 fn main() {
     icfpc2018_lib::dummy();
-    println!("Hello, world!");
+    env_logger::init();
+    match run() {
+        Ok(()) =>
+            info!("graceful shutdown"),
+        Err(e) => {
+            error!("fatal error: {:?}", e);
+            process::exit(1);
+        },
+    }
+}
+
+#[derive(Debug)]
+enum Error {
+    MissingParameter(&'static str),
+    Piston(PistonError),
+}
+
+#[derive(Debug)]
+enum PistonError {
+    BuildWindow(String),
+    LoadFont { file: String, error: io::Error, },
+    DrawText(gfx_core::factory::CombinedError),
+}
+
+const CONSOLE_HEIGHT: u32 = 32;
+const SCREEN_WIDTH: u32 = 640;
+const SCREEN_HEIGHT: u32 = 480;
+
+fn run() -> Result<(), Error> {
+    let matches = app_from_crate!()
+        .arg(Arg::with_name("assets-dir")
+             .short("a")
+             .long("assets-dir")
+             .value_name("DIR")
+             .help("Graphics resources directory")
+             .default_value("./assets")
+             .takes_value(true))
+        .get_matches();
+
+    let assets_dir = matches.value_of("assets-dir")
+        .ok_or(Error::MissingParameter("assets-dir"))?;
+
+    let opengl = OpenGL::V4_1;
+    let mut window: PistonWindow = WindowSettings::new("icfpc2018 visualizer", [SCREEN_WIDTH, SCREEN_HEIGHT])
+        .exit_on_esc(true)
+        .opengl(opengl)
+        .build()
+        .map_err(PistonError::BuildWindow)
+        .map_err(Error::Piston)?;
+
+    let mut font_path = PathBuf::from(assets_dir);
+    font_path.push("FiraSans-Regular.ttf");
+    let mut glyphs = Glyphs::new(&font_path, window.factory.clone(), TextureSettings::new())
+        .map_err(|e| Error::Piston(PistonError::LoadFont {
+            file: font_path.to_string_lossy().to_string(),
+            error: e,
+        }))?;
+
+    unimplemented!()
 }
