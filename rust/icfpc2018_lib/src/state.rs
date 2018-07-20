a40046fb7f0253c4454c146720ff6d511a538cce
Alexey Voznyuk <me@swizard.info>
2018-07-20 23:49:13 +0300

    `State::wellformed`

--- rust/icfpc2018_lib/src/state.rs
+++ rust/icfpc2018_lib/src/state.rs
@@ -1,4 +1,4 @@
-use std::collections::BTreeMap;
+use std::collections::{HashSet, BTreeMap};
 
 use super::{
     coord::{
@@ -34,4 +34,39 @@ pub struct State {
     pub matrix: Matrix,
     pub bots: BTreeMap<Bid, Bot>,
     pub trace: Vec<BotCommand>,
+}
+
+#[derive(Clone, Copy, PartialEq, Eq, Hash, Debug)]
+pub enum WellformedStatus {
+    Wellformed,
+    NotGroundedWhileLowHarmonics,
+    BotInFilledVoxel,
+    SeedsAreNotDisjoint,
+    SeedIsTheSameAsActiveBot,
+}
+
+
+impl State {
+    pub fn wellformed(&self) -> WellformedStatus {
+        if let Harmonics::Low = self.harmonics {
+            if !self.matrix.all_voxels_are_grounded() {
+                return WellformedStatus::NotGroundedWhileLowHarmonics;
+            }
+        }
+        if !self.bots.values().map(|bot| &bot.pos).all(|pos| !self.matrix.is_filled(pos)) {
+            return WellformedStatus::BotInFilledVoxel;
+        }
+
+        let mut bids_seen = HashSet::new();
+        for &bid in self.bots.values().flat_map(|bot| bot.seeds.iter()) {
+            if !bids_seen.insert(bid) {
+                return WellformedStatus::SeedsAreNotDisjoint;
+            }
+            if self.bots.contains_key(&bid) {
+                return WellformedStatus::SeedIsTheSameAsActiveBot;
+            }
+        }
+
+        WellformedStatus::Wellformed
+    }
 }
