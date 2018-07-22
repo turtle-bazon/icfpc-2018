9b8705e32d0b196e9088ff7e2bf4bb1efc194c61
Alexey Voznyuk <me@swizard.info>
2018-07-22 16:07:44 +0300

    initial `solver::random_swarm` code

--- rust/icfpc2018_lib/src/lib.rs
+++ rust/icfpc2018_lib/src/lib.rs
@@ -4,6 +4,7 @@ extern crate bit_vec;
 extern crate pathfinding;
 #[macro_use] extern crate itertools;
 
+pub mod solver;
 pub mod router;
 pub mod coord;
 pub mod state;
---
+++ rust/icfpc2018_lib/src/solver/random_swarm.rs
@@ -0,0 +1,85 @@
+use rand::{self, Rng};
+
+use super::super::{
+    coord::{
+        Coord,
+        Matrix,
+    },
+    cmd::BotCommand,
+    state::{
+        Bid,
+        Bot,
+    },
+};
+
+#[derive(Clone, PartialEq, Eq, Debug)]
+pub enum Error {
+
+}
+
+pub fn solve(source_model: Matrix, target_model: Matrix) -> Result<Vec<BotCommand>, Error> {
+
+    let mut nanobots = Nanobot::init();
+
+
+    unimplemented!()
+}
+
+#[derive(Clone, PartialEq, Eq, Debug)]
+struct Nanobot {
+    bid: Bid,
+    bot: Bot,
+}
+
+impl Nanobot {
+    fn init() -> Vec<Nanobot> {
+        vec![
+            Nanobot {
+                bid: 1,
+                bot: Bot {
+                    pos: Coord {
+                        x: 0,
+                        y: 0,
+                        z: 0,
+                    },
+                    seeds: (2 ..= 40).collect(),
+                },
+            },
+        ]
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::super::super::{
+        coord::Coord,
+        state::Bot,
+    };
+    use super::Nanobot;
+
+    #[test]
+    fn nanobot_init() {
+        assert_eq!(
+            Nanobot::init(),
+            vec![
+                Nanobot {
+                    bid: 1,
+                    bot: Bot {
+                        pos: Coord {
+                            x: 0,
+                            y: 0,
+                            z: 0,
+                        },
+                        seeds: vec![
+                            2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
+                            13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
+                            23, 24, 25, 26, 27, 28, 29, 30, 31, 32,
+                            33, 34, 35, 36, 37, 38, 39, 40,
+                        ],
+                    },
+                }
+            ],
+        );
+    }
+
+}
