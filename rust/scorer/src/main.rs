e67864a99cbc8f3c67fec974836fac3d96b8f763
Gleb Golubitsky <sectoid@gnolltech.org>
2018-07-23 11:38:50 +0300

    scorer: src/dst model args, result comparison. scorer in CI
    Signed-off-by: Gleb Golubitsky <sectoid@gnolltech.org>

--- rust/scorer/src/main.rs
+++ rust/scorer/src/main.rs
@@ -7,49 +7,74 @@ use std::io::Read;
 
 use icfpc2018_lib as kernel;
 
+use kernel::{
+    cmd,
+    model,
+    state,
+};
+
 #[derive(Debug)]
 enum Error {
     Args(clap::Error),
+    NoSourceOrTargetModelProvided,
     Io(std::io::Error),
-    ModelReadError(kernel::model::Error),
+    Model(kernel::model::Error),
     Cmd(kernel::cmd::Error),
     State(kernel::state::Error),
+    ModelNotMatch,
 }
 
 
 
 fn main() -> Result<(),Error> {
     let app = app_from_crate!()
-        .arg(Arg::with_name("model")
-             .display_order(1)
-             .short("m")
-             .long("model")
-             .help("Model file (In)",)
-             .default_value("../../problems/FA001_tgt.mdl")
+        .arg(Arg::with_name("source-model")
+             .short("s")
+             .long("source-model")
+             .value_name("FILE")
+             .help("Source model")
+             .takes_value(true))
+        .arg(Arg::with_name("dst-model")
+             .short("d")
+             .long("dst-model")
+             .value_name("FILE")
+             .help("Destination model")
              .takes_value(true))
         .arg(Arg::with_name("trace")
              .display_order(1)
              .short("t")
              .long("trace")
-             .help("Original trace (In)",)
-             .default_value("../../traces/FA001.nbt")
+             .help("Trace",)
              .takes_value(true));
 
     let matches = app.get_matches();
-    let model_filename = value_t!(matches, "model", String).map_err(Error::Args)?;
-    let trace_filename = value_t!(matches, "trace", String).map_err(Error::Args)?;
+    let (source_model, dst_model) =
+        if let Some(source_model_file) = matches.value_of("source-model") {
+            if let Some(dst_model_file) = matches.value_of("dst-model") {
+                (model::read_model_file(source_model_file).map_err(Error::Model)?,
+                 model::read_model_file(dst_model_file).map_err(Error::Model)?)
+            } else {
+                let source_model = model::read_model_file(source_model_file).map_err(Error::Model)?;
+                let dst_model = source_model.new_empty_of_same_size();
+                (source_model, dst_model)
+            }
+        } else {
+            if let Some(dst_model_file) = matches.value_of("dst-model") {
+                let dst_model = model::read_model_file(dst_model_file).map_err(Error::Model)?;
+                let source_model = dst_model.new_empty_of_same_size();
+                (source_model, dst_model)
+            } else {
+                return Err(Error::NoSourceOrTargetModelProvided);
+            }
+        };
 
-    let ref_model = match kernel::model::read_model_file(model_filename) {
-        Err(e) => return Err(Error::ModelReadError(e)),
-        Ok(m) => m,
-    };
+    let trace_filename = value_t!(matches, "trace", String).map_err(Error::Args)?;
 
     let mut f = File::open(&trace_filename).map_err(Error::Io)?;
     let mut buffer = Vec::new();
     f.read_to_end(&mut buffer).map_err(Error::Io)?;
 
-    let matrix = ref_model.new_empty_of_same_size();
-    let mut state = kernel::state::State::new(matrix, vec![]);
+    let mut state = kernel::state::State::new(source_model, vec![]);
 
     let cmds = kernel::cmd::from_bytes(&buffer).map_err(Error::Cmd)?;
     println!("Commands: {}", cmds.len());
@@ -63,6 +88,17 @@ fn main() -> Result<(),Error> {
             Err(Error::State(e))
         },
         Ok(_) => {
+            for voxel in state.matrix.filled_voxels() {
+                if state.matrix.is_filled(voxel) && !dst_model.is_filled(voxel) {
+                    return Err(Error::ModelNotMatch);
+                }
+            }
+            for voxel in dst_model.filled_voxels() {
+                if !state.matrix.is_filled(voxel) {
+                    return Err(Error::ModelNotMatch);
+                }
+            }
+
             println!("SUCCESS");
             Ok(())
         }
