1ee4e3b8cf07b9a561b6ffc9967d344e5f0812e6
Gleb Golubitsky <sectoid@gnolltech.org>
2018-07-23 18:37:04 +0300

    swarm: traces and scripts
    Signed-off-by: Gleb Golubitsky <sectoid@gnolltech.org>

--- tools/score-fa.sh
+++ tools/score-fa.sh
@@ -6,22 +6,38 @@ TRACE=$1
 MODEL=${TRACE/traces/problems}
 MODEL=${MODEL/.nbt/_tgt.mdl}
 
-(rust/scorer/target/release/scorer -t $TRACE -d $MODEL | grep 'FINAL ENERGY' | cut -f4 -d ' ') \
-    | read ORIG_SCORE
+echo "$TRACE: Start processing"
+ORIG_SCORE=$(rust/scorer/target/release/scorer -t $TRACE -d $MODEL | grep 'FINAL ENERGY' | cut -f4 -d ' ')
+if [ "$ORIG_SCORE" == "" ]; then
+    echo "$TRACE: unable to score orig trace"
+    exit 1
+fi
 ORIG_SCORE=$(($ORIG_SCORE+0))
+echo "$TRACE: Orig score: $ORIG_SCORE"
 
 NEW_TRACE=`mktemp`
 
+echo "$TRACE: Solving"
+
 rust/random_swarm/target/release/random_swarm \
     -t $MODEL -o $NEW_TRACE \
+    -M 12 \
     --global-ticks-limit 131072 --rtt-limit 256 --route-attempts-limit 512
 
-(rust/scorer/target/release/scorer -t $NEW_TRACE -d $MODEL | grep 'FINAL ENERGY' | cut -f4 -d ' ') \
-    | read NEW_SCORE
+
+NEW_SCORE=$(rust/scorer/target/release/scorer -t $NEW_TRACE -d $MODEL | grep 'FINAL ENERGY' | cut -f4 -d ' ')
+if [ "$NEW_SCORE" == "" ]; then
+    echo "$TRACE: unable to score the new trace"
+    echo "$TRACE: check $NEW_TRACE for debug"
+    exit 1
+fi
 NEW_SCORE=$(($NEW_SCORE+0))
+
+echo "$TRACE: was $ORIG_SCORE now $NEW_SCORE"
 
 if [[ $NEW_SCORE -lt $OLD_SCORE ]];
 then
     echo "IMPROVED $TRACE: was $ORIG_SCORE now $NEW_SCORE"
     mv -f $NEW_TRACE $TRACE
 fi
+rm -f $NEW_TRACE
--- tools/score-fr.sh
+++ tools/score-fr.sh
@@ -7,19 +7,34 @@ MODEL=${TRACE/traces/problems}
 MODEL_SRC=${MODEL/.nbt/_src.mdl}
 MODEL_DST=${MODEL/.nbt/_tgt.mdl}
 
-(rust/scorer/target/release/scorer -t $TRACE -s $MODEL_SRC -d $MODEL_DST | grep 'FINAL ENERGY' | cut -f4 -d ' ') \
-    | read ORIG_SCORE
+echo "$TRACE: Start processing"
+ORIG_SCORE=$(rust/scorer/target/release/scorer -t $TRACE -s $MODEL_SRC -d $MODEL_DST | grep 'FINAL ENERGY' | cut -f4 -d ' ')
+if [ "$ORIG_SCORE" == "" ]; then
+    echo "$TRACE: unable to score orig trace"
+    exit 1
+fi
+
+
 ORIG_SCORE=$(($ORIG_SCORE+0))
+echo "$TRACE: Orig score: $ORIG_SCORE"
 
 NEW_TRACE=`mktemp`
 
+echo "$TRACE: Solving"
 rust/random_swarm/target/release/random_swarm \
     -s $MODEL_SRC -t $MODEL_DST -o $NEW_TRACE \
+    -M 12 \
     --global-ticks-limit 131072 --rtt-limit 256 --route-attempts-limit 512
 
-(rust/scorer/target/release/scorer -t $NEW_TRACE -s $MODEL_SRC -d $MODEL_DST | grep 'FINAL ENERGY' | cut -f4 -d ' ') \
-    | read NEW_SCORE
+NEW_SCORE=$(rust/scorer/target/release/scorer -t $NEW_TRACE -s $MODEL_SRC -d $MODEL_DST | grep 'FINAL ENERGY' | cut -f4 -d ' ')
+if [ "$NEW_SCORE" == "" ]; then
+    echo "$TRACE: unable to score the new trace"
+    echo "$TRACE: check $NEW_TRACE for debug"
+    exit 1
+fi
 NEW_SCORE=$(($NEW_SCORE+0))
+
+echo "$TRACE: was $ORIG_SCORE now $NEW_SCORE"
 
 if [ ${NEW_SCORE} -lt ${ORIG_SCORE} ]
 then
