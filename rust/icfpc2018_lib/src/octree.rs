636d7bc9c7de3f9e68ee5afb833702a6fc6c148c
Alexey Voznyuk <me@swizard.info>
2018-07-25 02:28:38 +0300

    working `Octree::nearest`

--- rust/icfpc2018_lib/src/octree.rs
+++ rust/icfpc2018_lib/src/octree.rs
@@ -1,8 +1,10 @@
 use std::mem;
+use std::collections::BinaryHeap;
 
 use super::coord::{
     Region,
     Coord,
+    CoordDiff,
 };
 
 #[derive(Clone, PartialEq, Eq, Debug)]
@@ -18,6 +20,7 @@ pub enum InsertStatus {
 
 pub struct Octree<T> {
     nodes: Vec<Node<T>>,
+    queue: BinaryHeap<(isize, usize)>,
 }
 
 
@@ -41,6 +44,7 @@ impl<T> Octree<T> {
                 bounds: dimension,
                 content: Content::Empty,
             }],
+            queue: BinaryHeap::new(),
         }
     }
 
@@ -220,7 +224,6 @@ impl<T> Octree<T> {
             if !node.bounds.contains(&point) {
                 return None;
             }
-            let mid = bounds_mid(&node.bounds);
 
             match node.content {
                 Content::Empty =>
@@ -229,10 +232,74 @@ impl<T> Octree<T> {
                     return Some(value),
                 Content::Leaf { .. } =>
                     return None,
-                Content::Tree(ref nodes) =>
-                    node_ref = nodes[leaf_index(&mid, &point)],
+                Content::Tree(ref nodes) => {
+                    let mid = bounds_mid(&node.bounds);
+                    node_ref = nodes[leaf_index(&mid, &point)];
+                },
             }
         }
+    }
+
+    pub fn nearest<'a>(&'a mut self, needle: &Coord) -> Option<(usize, &'a Coord, &'a T)> where T: ::std::fmt::Debug {
+
+        self.queue.clear();
+        self.queue.push((0, 0));
+
+        let mut best_found: Option<(usize, &'a Coord, &'a T)> = None;
+
+        while let Some((node_distance_neg, node_ref)) = self.queue.pop() {
+            let node_distance = -node_distance_neg as usize;
+            if best_found.as_ref().map(|best| best.0 < node_distance).unwrap_or(false) {
+                break;
+            }
+
+            let node = &self.nodes[node_ref];
+            match node.content {
+                Content::Empty =>
+                    (),
+                Content::Leaf { ref point, ref value, } if point == needle =>
+                    return Some((0, point, value)),
+                Content::Leaf { ref point, ref value, } => {
+                    let distance = needle.diff(point).l_1_norm();
+                    if best_found.as_ref().map(|best| distance < best.0).unwrap_or(true) {
+                        best_found = Some((distance, point, value));
+                    }
+                },
+                Content::Tree(ref children) => {
+                    let nodes = &self.nodes;
+                    self.queue.extend(children.iter().map(|&node_ref| {
+                        let node = &nodes[node_ref];
+                        let proj_diff = CoordDiff(Coord {
+                            x: if needle.x < node.bounds.min.x {
+                                node.bounds.min.x - needle.x
+                            } else if needle.x > node.bounds.max.x {
+                                needle.x - node.bounds.max.x
+                            } else {
+                                0
+                            },
+                            y: if needle.y < node.bounds.min.y {
+                                node.bounds.min.y - needle.y
+                            } else if needle.y > node.bounds.max.y {
+                                needle.y - node.bounds.max.y
+                            } else {
+                                0
+                            },
+                            z: if needle.z < node.bounds.min.z {
+                                node.bounds.min.z - needle.z
+                            } else if needle.z > node.bounds.max.z {
+                                needle.z - node.bounds.max.z
+                            } else {
+                                0
+                            },
+                        });
+                        let node_distance_neg = -(proj_diff.l_1_norm() as isize);
+                        (node_distance_neg, node_ref)
+                    }));
+                },
+            }
+        }
+
+        best_found
     }
 }
 
@@ -283,5 +350,40 @@ mod test {
             assert_eq!(tree.get(point), Some(value));
         }
         assert_eq!(tree.get(&Coord { x: 0, y: 0, z: 0, }), None);
+    }
+
+    #[test]
+    fn nearest_1000() {
+        use std::collections::HashMap;
+
+        let mut tree =
+            Octree::new(Region { min: Coord { x: 0, y: 0, z: 0, }, max: Coord { x: 250, y: 250, z: 250, }, });
+
+        let mut rng = rand::thread_rng();
+        let samples: HashMap<_, _> = (0 .. 1000)
+            .map(|_| (Coord {
+                x: rng.gen_range(0, 251),
+                y: rng.gen_range(0, 251),
+                z: rng.gen_range(0, 251),
+            }, rng.gen_range(-1000, 1000)))
+            .collect();
+        for (&point, &value) in samples.iter() {
+            tree.insert(point, value).unwrap();
+        }
+
+        for _ in 0 .. 1000 {
+            let needle = Coord {
+                x: rng.gen_range(0, 251),
+                y: rng.gen_range(0, 251),
+                z: rng.gen_range(0, 251),
+            };
+
+            let expected = samples
+                .iter()
+                .map(|(p, v)| (needle.diff(p).l_1_norm(), p, v))
+                .min_by_key(|r| r.0);
+            let result = tree.nearest(&needle);
+            assert_eq!(result.map(|r| r.0), expected.map(|r| r.0));
+        }
     }
 }
